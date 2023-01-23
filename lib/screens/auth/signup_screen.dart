27ec132b5464209de222f178b46519befe24e322
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-01-23 23:20:39 +0100

    add style to signup_screen.dart

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -7,33 +7,150 @@ class SignupScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('title'),
+          title: Text(
+            'Ecommerce App',
+          ),
         ),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Icon(Icons.arrow_back_ios_new_rounded),
-                Text("Sign up")
-              ],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(35),
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        size: 16,
+                        color: Colors.grey,
+                      ),
+                      Text("Sign up",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500)),
+                      Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        size: 16,
+                        color: Colors.white,
+                      )
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                  SizedBox(
+                    height: 45,
+                  ),
+                  Text(
+                    "Register Account",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    "Complete your details or continue \nwith social media",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      height: 1.5,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        label: Text("Email"),
+                        hintText: "Enter your email",
+                        // floatingLabelBehavior: FloatingLabelBehavior.always,
+                        suffixIcon: Icon(Icons.email_outlined),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50)))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        label: Text("Password"),
+                        hintText: "Enter your password",
+                        suffixIcon: Icon(Icons.lock_outline),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50)))),
+                    obscureText: true, //hide password
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        label: Text("Confirm Password"),
+                        hintText: "Re-enter your password",
+                        suffixIcon: Icon(Icons.lock_outline),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50)))),
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        foregroundColor: Colors.white,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 130, vertical: 20),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      ), //better than ButtonStyle cuz it removes all styles
+                      onPressed: () => print("salam"),
+                      child: Text("Continue")),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                            color: Colors.grey, shape: BoxShape.circle),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                            color: Colors.grey, shape: BoxShape.circle),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                            color: Colors.grey, shape: BoxShape.circle),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "By continuing you confirm to agree with our Terms and conditions",
+                    style: TextStyle(color: Colors.grey),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
-            Text("Register Account"),
-            Text("Complete your details or continue \nwith social media"),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            TextButton(
-                onPressed: () => print("salam"), child: Text("Continue")),
-            Row(
-              children: [
-                Container(width: 14, height: 14, color: Colors.blue),
-                Container(width: 14, height: 14, color: Colors.blue),
-                Container(width: 14, height: 14, color: Colors.blue)
-              ],
-            ),
-            Text(
-                "By continuing you confirm to agree with our Terms and conditions"),
-          ],
+          ),
         ));
   }
 }
