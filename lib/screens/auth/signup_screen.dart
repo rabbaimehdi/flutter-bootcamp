de55d89b8aa93a60217fbd2211f76eb8c2444553
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-01-25 22:53:54 +0100

    update signup_screen, add colors and icons

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -6,151 +6,155 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Ecommerce App',
-          ),
-        ),
+        //   appBar: AppBar(title: Text('Ecommerce App')),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(35),
-            child: SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              child: Column(
+      child: Padding(
+        padding: const EdgeInsets.all(35),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.arrow_back_ios_new_rounded,
-                        size: 16,
-                        color: Colors.grey,
-                      ),
-                      Text("Sign up",
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500)),
-                      Icon(
-                        Icons.arrow_back_ios_new_rounded,
-                        size: 16,
-                        color: Colors.white,
-                      )
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  ),
-                  SizedBox(
-                    height: 45,
+                  Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    size: 16,
+                    color: Colors.grey,
                   ),
                   Text(
-                    "Register Account",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(
-                    "Complete your details or continue \nwith social media",
+                    "Sign up",
                     style: TextStyle(
                       color: Colors.grey,
-                      height: 1.5,
+                      fontSize: 15,
                       fontWeight: FontWeight.w500,
+                      fontFamily: "Ubuntu",
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                  SizedBox(
-                    height: 30,
+                  Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    size: 16,
+                    color: Colors.white,
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              Text(
+                "Register Account",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Ubuntu",
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                "Complete your details or continue \nwith social media",
+                style: TextStyle(
+                  color: Colors.grey,
+                  height: 1.5,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    label: Text("Email"),
+                    hintText: "Enter your email",
+                    // floatingLabelBehavior: FloatingLabelBehavior.always,
+                    suffixIcon: Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    label: Text("Password"),
+                    hintText: "Enter your password",
+                    suffixIcon: Icon(Icons.lock_outline),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)))),
+                obscureText: true, //hide password
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    label: Text("Confirm Password"),
+                    hintText: "Re-enter your password",
+                    suffixIcon: Icon(Icons.lock_outline),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)))),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xfff77546),
+                    foregroundColor: Colors.white,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 130, vertical: 20),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                  ), //better than ButtonStyle cuz it removes all styles
+                  onPressed: () => print("salam"),
+                  child: Text("Continue")),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 35,
+                    height: 35,
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                        color: Color(0xffeeeff1), shape: BoxShape.circle),
+                    child: Image.asset('assets/icons/001-google.png'),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        label: Text("Email"),
-                        hintText: "Enter your email",
-                        // floatingLabelBehavior: FloatingLabelBehavior.always,
-                        suffixIcon: Icon(Icons.email_outlined),
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(50)))),
+                  SizedBox(width: 10),
+                  Container(
+                    width: 35,
+                    height: 35,
+                    decoration: BoxDecoration(
+                        color: Color(0xffeeeff1), shape: BoxShape.circle),
+                    child: Image.asset('assets/icons/002-facebook.png'),
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        label: Text("Password"),
-                        hintText: "Enter your password",
-                        suffixIcon: Icon(Icons.lock_outline),
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(50)))),
-                    obscureText: true, //hide password
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        label: Text("Confirm Password"),
-                        hintText: "Re-enter your password",
-                        suffixIcon: Icon(Icons.lock_outline),
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(50)))),
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.orange,
-                        foregroundColor: Colors.white,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 130, vertical: 20),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30)),
-                      ), //better than ButtonStyle cuz it removes all styles
-                      onPressed: () => print("salam"),
-                      child: Text("Continue")),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 35,
-                        height: 35,
-                        decoration: BoxDecoration(
-                            color: Colors.grey, shape: BoxShape.circle),
-                      ),
-                      SizedBox(width: 10),
-                      Container(
-                        width: 35,
-                        height: 35,
-                        decoration: BoxDecoration(
-                            color: Colors.grey, shape: BoxShape.circle),
-                      ),
-                      SizedBox(width: 10),
-                      Container(
-                        width: 35,
-                        height: 35,
-                        decoration: BoxDecoration(
-                            color: Colors.grey, shape: BoxShape.circle),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "By continuing you confirm to agree with our Terms and conditions",
-                    style: TextStyle(color: Colors.grey),
-                    textAlign: TextAlign.center,
-                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    width: 35,
+                    height: 35,
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                        color: Color(0xffeeeff1), shape: BoxShape.circle),
+                    child: Image.asset('assets/icons/003-twitter.png'),
+                  )
                 ],
               ),
-            ),
+              SizedBox(height: 10),
+              Text(
+                "By continuing you confirm to agree with our Terms and conditions",
+                style: TextStyle(color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    ));
   }
 }
