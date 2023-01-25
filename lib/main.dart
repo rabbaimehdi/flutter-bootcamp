e952641bee38ca503a66ffe85df819fe9c309e2d
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-01-26 00:32:42 +0100

    change global font to Muli

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         fontFamily:
-            "Ubuntu", //apply this font in all text unless the font is precised
+            "Muli", //apply this font in all text unless the font is precised
         primarySwatch: Colors.orange,
       ),
       home: const OnboardingScreen(),
